a5152913c4ad0db0e9b256f95a55a551de52417c
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2025-01-31 22:49:47 +0100

    add functions for the controller

--- app/controllers/ControladorAlumnos.php
+++ app/controllers/ControladorAlumnos.php
@@ -8,20 +8,35 @@ class ControladorAlumnos
     private AlumnosDAO $alumnosObj;
 
     public function __construct() {
-        $this->view = 'ligaLocal';
+        $this->view = 'clasificacion';
         $this->page_title = 'Liga de Ajedrez';
         $this->alumnosObj = new AlumnosDAO();
     }
 
-    // Devuelve todos los alumnos
-    public function list() {
-        $this->page_title = 'Alumnos';
-        return $this->alumnosObj->getAlumnos();
+    public function inicio() {
+        $this->page_title = 'Inicio';
+        $this->view = 'inicio';
     }
 
-    // Devuelve los datos de un alumno
-    public function view($id) {
-        $this->page_title = 'Alumno';
-        return $this->alumnosObj->getAlumno($id);
+    public function ligaLocal() {
+        $this->page_title = 'Liga Local';
+        $dataToView['data'] = $this->alumnosObj->getAlumnos();
+
+        // Filtrar solo alumnos de LIGA LOCAL
+        $dataToView['data'] = array_filter($dataToView['data'], function ($alumno) {
+            return $alumno['categoria'] === 'LIGA LOCAL';
+        });
+        require_once '../app/views/clasificacion.php'; // Vista única
+    }
+
+    public function ligaInfantil() {
+        $this->page_title = 'Liga Infantil';
+        $dataToView['data'] = $this->alumnosObj->getAlumnos();
+
+        // Filtrar solo alumnos de LIGA INFANTIL
+        $dataToView['data'] = array_filter($dataToView['data'], function ($alumno) {
+            return $alumno['categoria'] === 'LIGA INFANTIL';
+        });
+        require_once '../app/views/clasificacion.php';
     }
 }
