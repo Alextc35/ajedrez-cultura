0db4a8777f9e6404b4061a5810c628ed3aa149c8
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2025-01-31 10:51:25 +0100

    Create ControladorAlumnos.php

---
+++ controller/ControladorAlumnos.php
@@ -0,0 +1,21 @@
+<?php
+require_once '/model/AlumnosDAO.php';
+
+class ControladorAlumnos
+{
+    public string $page_title;
+    public string $view;
+    private AlumnosDAO $alumnosObj;
+
+    public function __construct() {
+        $this->view = 'ligaLocal';
+        $this->page_title = 'Liga de Ajedrez';
+        $this->alumnosObj = new AlumnosDAO();
+    }
+
+    // Devuelve todos los alumnos
+    public function index() {
+        $this->page_title = 'Alumnos';
+        return $this->alumnosObj->getAll();
+    }
+}
