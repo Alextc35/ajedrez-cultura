9080f25a4ef8eaceb5699fdfa10555d73a46680c
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2025-01-31 10:50:49 +0100

    add templates

---
+++ view/templates/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC</title>
+</head>
+<body>
+    <header>
+        <div>
+            <h1><?= $controller->page_title ?></h1>
+        </div>
+    </header>
+    
