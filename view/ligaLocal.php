442e5587fbec3519e14c2256840552c302d7aac1
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2025-01-31 13:03:59 +0100

    Add view ligaLocal

--- view/ligaLocal.php
+++ view/ligaLocal.php
@@ -1,1 +1,41 @@
-hola
+<div>
+    <div>
+        <a href="index.php?controller=ControladorAlumnos&action=index">Ejemplo</a>
+        <hr/>
+    </div>
+    <div>
+        <?php
+        $cont = 1;
+        if (count($dataToView['data']) > 0) {
+            ?>
+            <table>
+            <tr>
+                <th>Posición</th>
+                <th>Nombre</th>
+                <th>Categoria</th>
+                <th>Victorias</th>
+                <th>Derrotas</th>
+                <th>Tablas</th>
+                <th>Puntos</th>
+            </tr>
+            <?php
+            foreach ($dataToView['data'] as $alumno) {
+                ?>
+                    <tr>
+                        <td><?= $cont++ ?></td>
+                        <td><?php echo $alumno['nombre']; ?></td>
+                        <td><?php echo $alumno['categoria']; ?></td>
+                        <td><?php echo $alumno['victorias']; ?></td>
+                        <td><?php echo $alumno['derrotas']; ?></td>
+                        <td><?php echo $alumno['tablas']; ?></td>
+                        <td><?php echo $alumno['victorias'] + $alumno['tablas'] / 2; ?></td>
+                    </tr>
+            <?php
+            }
+        } else {
+            echo 'No hay alumnos';
+        }
+    ?>
+        </table>
+    </div>
+</div>
