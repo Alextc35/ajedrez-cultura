f69e4cae8e78d5c42af5c5efd303441e8b4346ef
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2025-01-31 22:48:00 +0100

    create inicio.php (view)

---
+++ app/views/inicio.php
@@ -0,0 +1,9 @@
+<div class="container d-flex justify-content-center align-items-center">
+    <div class="text-center" style="background-color: #FFF;border: 1px solid #ccc; padding: 20px; border-radius: 10px;">
+        <h1>Selecciona la Liga</h1>
+        <p>Elige la liga a la que deseas acceder:</p>
+
+        <a href="?controller=ControladorAlumnos&action=ligaLocal" class="btn btn-primary">LIGA LOCAL</a>
+        <a href="?controller=ControladorAlumnos&action=ligaInfantil" class="btn btn-success">LIGA INFANTIL</a>
+    </div>
+</div>
