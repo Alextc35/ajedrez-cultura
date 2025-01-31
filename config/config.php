757e2b9ea6bd62cbb520c133320fbbfc00de1d41
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2025-01-31 13:03:10 +0100

    Update config.php
    rename variable db_name

--- config/config.php
+++ config/config.php
@@ -1,6 +1,10 @@
 <?php
     // Párametros de la base de datos
     const DB_HOST = 'localhost';
-    const DB = 'ajedrez_clase';
+    const DB_NAME = 'ajedrez_clase';
     const DB_USER = 'root';
     const DB_PASS = '';
+
+    // Párametros de la aplicación
+    const DEFAULT_CONTROLLER = "ControladorAlumnos";
+    const DEFAULT_ACTION = "index";
