1c955adffc892412845060cf2c8fe28d2a2f4b59
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2025-01-31 10:52:13 +0100

    test

---
+++ model/alumnos.sql
@@ -0,0 +1,9 @@
+CREATE TABLE alumnos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    categoria ENUM('LIGA LOCAL', 'LIGA INFANTIL') NOT NULL,
+    victorias INT DEFAULT 0,
+    derrotas INT DEFAULT 0,
+    tablas INT DEFAULT 0,
+    puntos INT DEFAULT 0
+);
