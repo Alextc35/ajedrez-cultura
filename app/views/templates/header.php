e66fbff23d5f903f82e2fc2275353e55cca486d9
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2025-02-01 20:32:42 +0100

    bootstrap icons

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $controller->page_title ?></title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/bootstrap-icons/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
