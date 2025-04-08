c8e6e98bb096fa71577a6c47f235b16eac90ac06
Alejandro Téllez Corona <atellezc35@gmail.com>
2025-04-08 19:32:04 +0200

    Cambio de nombre
    chess-league-management -> ajedrez-cultura

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,6 +1,6 @@
 <?php
     $paginaActual = $arrHandler['action'] ?? ''; // Obtener la acción actual
-
+    $index = constant('DEFAULT_INDEX');
     // Si el usuario no está autenticado o está en 'assign', deshabilitamos los enlaces
     $deshabilitado = $paginaActual === 'generateMatches' || !isset($_SESSION['usuario']) ? 'disabled' : '';
 ?>
@@ -10,16 +10,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $controller->page_title ?></title>
-    <link rel="stylesheet" href="/chess-league/public/assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/chess-league/public/assets/bootstrap/bootstrap-icons/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="/chess-league/public/assets/css/style.min.css">
+    <link rel="stylesheet" href="/ajedrez-cultura/public/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/ajedrez-cultura/public/assets/bootstrap/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/ajedrez-cultura/public/assets/css/style.min.css">
 </head>
 <body>
     <div class="d-flex flex-column justify-content-center align-items-center w-auto m-3">
         <div class="container text-center rounded shadow bg-light p-2 m-2">
             <h1>Selecciona la Liga</h1>
             <p>Elige la liga a la que deseas acceder:</p>
-            <a href="<?= constant('DEFAULT_INDEX')?>ControladorAlumnos/list?liga=LIGA LOCAL" class="btn btn-primary <?= $deshabilitado ?>" <?= $deshabilitado ? 'tabindex="-1" aria-disabled="true"' : '' ?>>LIGA LOCAL</a>
-            <a href="<?= constant('DEFAULT_INDEX')?>ControladorAlumnos/list?liga=LIGA INFANTIL" class="btn btn-success <?= $deshabilitado ?>" <?= $deshabilitado ? 'tabindex="-1" aria-disabled="true"' : '' ?>>LIGA INFANTIL</a>
+            <a href="<?= $index ?>ControladorAlumnos/list?liga=LIGA LOCAL" class="btn btn-primary <?= $deshabilitado ?>" <?= $deshabilitado ? 'tabindex="-1" aria-disabled="true"' : '' ?>>LIGA LOCAL</a>
+            <a href="<?= $index ?>ControladorAlumnos/list?liga=LIGA INFANTIL" class="btn btn-success <?= $deshabilitado ?>" <?= $deshabilitado ? 'tabindex="-1" aria-disabled="true"' : '' ?>>LIGA INFANTIL</a>
             <p class="text-muted pt-2 ">Desarrollado por <a href="https://www.linkedin.com/in/alejandrotellezcorona/" target="_blank" class="text-decoration-none text-muted fw-bold">Alejandro Téllez Corona</a></p>
         </div>
